04b4bfce42c674048c72171e8bdb2500c9ae21de
Brad Russell <brad.russell@pm.me>
2022-11-17 16:39:10 +1100

    day1

---
+++ 2019/day1.py
@@ -0,0 +1,25 @@
+import utils
+import math
+
+RAW = utils.get_input(1)
+DATA = list(utils.extract_ints(RAW))
+
+def calc_fuel(mass: int):
+    return max(math.floor(mass / 3) - 2, 0)
+
+def collapse(mass: int, total: int = 0):
+    remaining = calc_fuel(mass)
+    if remaining == 0:
+        return total
+    else:
+        return collapse(remaining, total+remaining)
+
+def part_one():
+    return sum(map(calc_fuel, DATA))
+
+def part_two():
+    return sum(map(collapse, DATA))
+
+if __name__ == "__main__":
+    print("part 1:", part_one())
+    print("part 2:", part_two())
