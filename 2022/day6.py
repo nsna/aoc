f88075bfa5061f5326fdc67654f996685dea5628
Brad Russell <brad.russell@pm.me>
2022-12-06 16:34:37 +1100

    day 6 modification

--- 2022/day6.py
+++ 2022/day6.py
@@ -1,16 +1,16 @@
 from utils import day
-from more_itertools import sliding_window
+from more_itertools import sliding_window, all_unique
 
 RAW = day(6)
 
 # part1
 for i, window in enumerate(sliding_window(RAW, 4)):
-    if len(set(window)) == 4:
+    if all_unique(window):
         print(i + 4)
         break
         
 # part2
 for i, window in enumerate(sliding_window(RAW, 14)):
-    if len(set(window)) == 14:
+    if all_unique(window):
         print(i + 14)
         break
