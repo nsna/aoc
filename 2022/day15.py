30ba1b990f187d8fc0436310dd2894512d7dbd4f
Brad Russell <brad.russell@pm.me>
2023-10-16 16:33:28 +1100

    day15

--- 2022/day15.py
+++ 2022/day15.py
@@ -1,63 +1,59 @@
-from utils import ints
+from utils import ints, manhattan_distance, day
 
-RAW = """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-Sensor at x=9, y=16: closest beacon is at x=10, y=16
-Sensor at x=13, y=2: closest beacon is at x=15, y=3
-Sensor at x=12, y=14: closest beacon is at x=10, y=16
-Sensor at x=10, y=20: closest beacon is at x=10, y=16
-Sensor at x=14, y=17: closest beacon is at x=10, y=16
-Sensor at x=8, y=7: closest beacon is at x=2, y=10
-Sensor at x=2, y=0: closest beacon is at x=2, y=10
-Sensor at x=0, y=11: closest beacon is at x=2, y=10
-Sensor at x=20, y=14: closest beacon is at x=25, y=17
-Sensor at x=17, y=20: closest beacon is at x=21, y=22
-Sensor at x=16, y=7: closest beacon is at x=15, y=3
-Sensor at x=14, y=3: closest beacon is at x=15, y=3
-Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
+RAW = day(15)
 
-SENSOR = 1
-BEACON = 2
+NOT_FOUND = 0
 
+class BeaconDict(dict):
+    def __missing__(self, key):
+        return NOT_FOUND
+    
 def parse():
-    cave = {}
+    sensors = {}
+    beacons = BeaconDict()
     for sensor in RAW.splitlines():
         x1, y1, x2, y2 = ints(sensor)
-        cave[x1, y1] = SENSOR
-        cave[x2, y2] = BEACON
-    return cave
+        # map sensor: distance_to_beacon
+        sensors[x1, y1] = manhattan_distance((x1, y1), (x2, y2))
+        beacons[x2, y2] = 1
+    return sensors, beacons
 
-def print_cave():
-    min_x = min(key[0] for key in cave.keys())
-    max_x = max(key[0] for key in cave.keys())
-    min_y = min(key[1] for key in cave.keys())
-    max_y = max(key[1] for key in cave.keys())
+def part1(line):
+    left = right = 0
+    for x, y in sensors:
+        dist_from_line = abs(line - y)
+        intersect = sensors[(x, y)] - dist_from_line
+        if intersect >= 0:
+            left = min(left, x - intersect)
+            right = max(right, x + intersect + 1)
 
-    width = max_x - min_x + 1
-    height = max_y - min_y + 1
+    # remove any beacons on the line
+    right -= sum(beacon for (_, y), beacon in beacons.items() if y == line)
+    print(right-left)
 
-    grid = [['.' for _ in range(width)] for _ in range(height)]
-    for (x, y), value in cave.items():
-        if value == SENSOR:
-            grid[y - min_y][x - min_x] = 'S'
-        elif value == BEACON:
-            grid[y - min_y][x - min_x] = 'B'
-    
-    for row in grid:
-        print(''.join(row))
+def part2(depth):
+    for line in range(depth + 1):
+        scans = []
+        for x, y in sensors:
+            dist_from_line = abs(line - y)
+            intersect = sensors[(x, y)] - dist_from_line
+            if intersect >= 0:
+                scans.append((x - intersect, x + intersect))
+        # sort the ranges so it's easier to merge
+        scans.sort()
+        _, right = scans.pop(0)
+        # attempt to merge the scan ranges, break if not continuous
+        for start, end in scans:
+            continuous = True
+            if start - right > 1:
+                continuous = False
+                print((right + 1) * 4000000 + line)
+                break
+            else:
+                right = max(right, end)
+        if not continuous:
+            break
 
-
-def part1(line = 10):
-    # 1. on the given line find closest sensor
-    # 2. find number of positions N on the line the sensor field encorporates
-    # 3. find next closest sensor to the line
-    # 4. repeat until no changes to N occur
-    candidates = sorted(
-        [key for key in cave.keys() if cave[key] == SENSOR], 
-        key = lambda xy: abs(line - xy[1]))
-    print(candidates)
-
-
-cave = parse()
-part1()
-#print_cave()
-#print(cave)
+sensors, beacons = parse()
+part1(2000000)
+part2(4000000)
