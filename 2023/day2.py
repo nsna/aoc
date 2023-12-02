eb202904bd9471cf460f79a41a463f456dc997cf
Brad Russell <brad.russell@pm.me>
2023-12-02 20:16:27 +1100

    refactor, forgot Counter and prod existed

--- 2023/day2.py
+++ 2023/day2.py
@@ -1,40 +1,25 @@
 import re
-
+from collections import Counter
 from utils import day
+from math import prod
 
 LINES = day(2).splitlines()
 
-def part1():
+def parse():
     pattern = r'(\d+) (red|green|blue)'
-    sum = 0
-    for game, line in enumerate(LINES, start=1):
-        bag = {
-            'red':  12, 'green': 13, 'blue':  14
-        }
-        for round in line.split(';'):
-            valid = True
-            for num, colour in re.findall(pattern, round):
-                if int(num) > bag[colour]:
-                    valid = False
-                    break
-            if not valid:
-                break
-        if valid:
-            sum += game
-    print(sum)
+    for game_num, game in enumerate(LINES, start=1):
+        most_cubes = Counter()
+        for num, colour in re.findall(pattern, game):
+            most_cubes[colour] = max(most_cubes[colour], int(num))
+        yield game_num, most_cubes
+
+def part1():
+    limit = Counter({'red':  12, 'green': 13, 'blue':  14})
+    print(sum((game_num for game_num, cubes in games if cubes < limit)))
 
 def part2():
-    pattern = r'(\d+) (red|green|blue)'
-    sum = 0
-    for line in LINES:
-        bag = {
-            'red':  0, 'green': 0, 'blue':  0
-        }
-        for round in line.split(';'):
-            for num, colour in re.findall(pattern, round):
-                bag[colour] = max(bag[colour], int(num))
-        sum += (bag['red'] * bag['green'] * bag['blue'])
-    print(sum)
+    print(sum(prod(cubes.values()) for _, cubes in games))
 
+games = list(parse())
 part1()
 part2()
