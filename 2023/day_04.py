8a6afb02f3e890f856e8baf88ef5f4d15a8e793d
Brad Russell <brad.russell@pm.me>
2023-12-04 21:45:31 +1100

    tidy up

--- 2023/day_04.py
+++ 2023/day_04.py
@@ -1,28 +1,22 @@
 from utils import day
-import re
-import time
+
 LINES = day(4).splitlines()
 
-start_time = time.time()
-
 def parse_cards():
-    wins = {}
-    for i, line in enumerate(LINES, start = 1):
-        numbers = re.findall(r'(\d+)', line)
-        wins[i] = len(set(numbers[1:11]) & set(numbers[11:]))
-    return wins
+    for line in LINES:
+        left, right = line.split(':')[1].split('|')
+        yield len(set(left.split()) & set(right.split()))
 
 def part1():
-    print(sum((2**(n - 1)) for n in wins.values() if n > 0))
+    print(sum((2**(n - 1)) for n in wins if n > 0))
 
 def part2():
-    sum_cards = dict(enumerate([1] * len(wins), start = 1))
-    for current_card, new_cards in wins.items():
+    sum_cards = [1] * len(wins)
+    for current_card, new_cards in enumerate(wins):
         for next_card in range(current_card + 1, current_card + new_cards + 1):
             sum_cards[next_card] += sum_cards[current_card]
-    print(sum(sum_cards.values()))
+    print(sum(sum_cards))
 
-wins = parse_cards()
+wins = list(parse_cards())
 part1()
 part2()
-print ('[Finished in {:.2f}ms]'.format(1000*(time.time() - start_time)))
