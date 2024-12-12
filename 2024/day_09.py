9720651a1d7fcde8454e00112cbf54f1ca76a182
Brad Russell <brad.russell@tic.toshiba.com.au>
2024-12-12 20:57:16 +1100

    gross but done

--- 2024/day_09.py
+++ 2024/day_09.py
@@ -1,10 +1,11 @@
 import re
 from itertools import cycle
+from collections import defaultdict
 
 from utils import day
 
 RAW = "2333133121414131402"
-#RAW = day(9)
+RAW = day(9)
 
 def expand(disk):
     fs = {}
@@ -22,6 +23,18 @@ def expand(disk):
             block += 1
     return fs
 
+def all_free_segments(fs):
+    free_blocks = defaultdict(int)
+    seq = None
+    for key, value in fs.items():
+        if value is None:
+            if seq is None:
+                seq = key
+            free_blocks[seq] += 1
+        else:
+            seq = None
+    return free_blocks
+
 def next_free_block(fs) -> int:
     return next((key for key, value in fs.items() if value is None), -1)
 
@@ -35,10 +48,6 @@ def next_free_segment(fs):
 def last_populated_block(fs):
     return next(reversed(sorted(k for k, v in fs.items() if v is not None)), -1)
 
-def next_file(fs, prev_file):
-    return next(reversed(sorted(k for k, v in fs.items() if v is not None)), -1)
-    ...
-
 def get_span(fs, start_block):
     block = start_block
     while fs[block] == fs[start_block]:
@@ -51,7 +60,7 @@ def get_reverse_span(fs, start_block):
         block -= 1
     return block + 1
 
-def compact(fs):
+def defrag(fs):
     while True:
         last_block = last_populated_block(fs)
         first_free = next_free_block(fs)
@@ -61,43 +70,36 @@ def compact(fs):
         fs[last_block] = None
     return fs
 
-def compact_sparse(fs):
-    file = fs[last_populated_block(fs)]
-    print(fs)
+def to_str(fs):
+    for k, v in fs.items():
+        if v is not None:
+            print(v, end="")
+        else:
+            print('.', end="")
     print()
+
+def defrag2(fs):
+    file = fs[last_populated_block(fs)]
     while file > 0:
-        print(file)
         file_end = next_block_tail(fs, file)
         file_start = get_reverse_span(fs, file_end)
         file_size = (file_end - file_start) + 1
-        free_start, free_size = next_free_segment(fs)
-
-        if file_size <= free_size:
+        free_segments = all_free_segments(fs)
+        free_start = next(iter(start for (start, size) in free_segments.items() if file_size <= size), None)
+        if (free_start is not None and free_start < file_start):
             for i in range(file_size):
                 fs[free_start + i] = fs[file_start + i]
                 fs[file_start + i] = None
         file -= 1
-    print(fs)
-    #print(file, file_start, file_size, free_start, free_size)
-    #last_file_block = last_populated_block(fs)
-    #file_start = get_reverse_span(fs, last_file_block)
-    #file_size = (last_file_block - file_start) + 1
-    #free_start, free_size = next_free_segment(fs)
-    #print(file)
-    #print(next_block_tail(fs, 8))
-
+    return fs
 
 def p1():
-    fs = compact(expand(RAW))
+    fs = defrag(expand(RAW))
     print(sum(block * file for (block, file) in fs.items() if file is not None))
 
 def p2():
-    fs = expand(RAW)
-    print(fs)
-    start, size = next_free_segment(fs)
-    print(start, size)
-    compact_sparse(fs)
-
+    fs = defrag2(expand(RAW))
+    print(sum(block * file for (block, file) in fs.items() if file is not None))
 
 p1()
 p2()
